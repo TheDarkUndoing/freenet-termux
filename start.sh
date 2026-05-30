9fd9de60fb654d8e41d8f9bc312e1270344413b5
h4ckmen <>
2026-05-30 02:04:22 -0400

    fixes

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo "Installing proot-distro..."
 # Needed to set it up env
 pkg install proot-distro
 
-proot-distro install ubuntu:24.04
-proot-distro login ubuntu -- bash -c "export DISPLAY=:1 && echo "Installing Freenet..." && curl -fsSL https://freenet.org/install.sh | sh && freenet network --log-dir=/tmp &>>/dev/null"
+proot-distro install ubuntu:24.04 --name=freenet-termux
+proot-distro login freenet-termux -- bash -c "export DISPLAY=:1 && echo "Installing Freenet..." && curl -fsSL https://freenet.org/install.sh | sh && freenet network --log-dir=/tmp &>>/dev/null"
 
 
